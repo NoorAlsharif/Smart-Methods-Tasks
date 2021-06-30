77835e5c962dae2dd3eef317c0ee91d2286119f7
Noor AlGhalib AlSharif <nalsharif0045@stu.kau.edu.sa>
2021-06-30 16:46:34 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,10 +8,11 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
+        <title>Fighting Robots</title>
 
         <style>
 
+            /* General Style */
             body {
                 background-color: darkblue;
                 background-image: url("backg.png");
@@ -21,7 +22,7 @@ and open the template in the editor.
 
             header {
                 margin-top: 20px;
-                margin-bottom: 20px;
+                /*margin-bottom: 20px;*/
             }
 
             h1 {
@@ -33,34 +34,45 @@ and open the template in the editor.
             }
 
             div {
+
+                text-align: center;
+                /*margin-top: 40px;*/
+                margin-left: auto;
+                margin-right: auto;
+                display: inline-block;
+
+
+            }
+
+            label {
+                width: 100%;
+                margin: auto;/*50px;*/
+                font-size: 35px;
+                font-family: Helvetica;
+            }
+        </style>
+
+        <style>
+            /* Robotic Arm Range */
+            div.rangeContainer {
+                border-radius: 80px;
+                background-image: linear-gradient(white, white, white, rgba(0,0,0,0));
+                height: 600px;
+                width: 50%;
+                margin-right: 1%;
+                /*margin-left: 1%;
+                margin-top: 1%;
+                margin-bottom: 1%;
+                /*height: 100%;*/
+            }
+
+            .slidecontainer {
+                width: 100%; /* Width of the outside container */
                 text-align: center;
                 margin-top: 40px;
                 margin-left: auto;
                 margin-right: auto;
                 display: inline-block;
-            }
-
-            label {
-                width: 100%;
-                margin: 50px;
-                font-size: 35px;
-                font-family: Helvetica;
-            }
-
-            .slidecontainer {
-                width: 100%; /* Width of the outside container */
-            }
-
-            div.outerbox {
-                border-radius: 80px;
-                text-align: center;
-                background-image: linear-gradient(white, white, white, rgba(0,0,0,0));
-                height: 600px;
-                width: 50%;
-                margin-left: 1%;
-                /*height: 100%;*/
-
-
             }
 
             .slider {
@@ -89,8 +101,54 @@ and open the template in the editor.
                 cursor: pointer; /* Cursor on hover */
 
             }
+        </style>
 
-            button {
+        <style>
+
+
+            /* Direction Buttons */
+            .directionContainer{
+                border-radius: 30px;
+                background-image: linear-gradient(white,white, rgba(0,0,0,0));
+                height:300px; /*300px;*/
+                width: 310px;
+                margin-right: 1%;
+                margin-top: 230px;
+            }
+
+            .directions{
+                text-align: center;
+            }
+
+            button.dirbutton {
+                background-image: linear-gradient(to right, #CA9DFF, #5B328D);
+                height: 43px;
+                width: 140px;
+                text-align: center;
+                border-radius: 18px;
+                font-size: 23px;
+                font-family: Helvetica;
+                font-weight: bold;
+                color: white;
+                display:block;
+                margin-bottom: 30px;
+                text-shadow: -2px 2px #332D2D;
+            }
+
+
+        </style>
+
+        <style>
+
+            /* Action Buttons */
+            .buttondiv {
+                /*margin-bottom: 1%;*/
+                margin-left: 1%;
+                /*text-align: right;*/
+
+            }
+
+            button.general {
                 height: 70px;
                 width: 300px;
                 padding: 5px 5px 5px 20px;
@@ -101,11 +159,12 @@ and open the template in the editor.
                 font-weight: bold;
                 color: white;
                 display: block;
-                margin-bottom: 20px;
+                margin-bottom: 50px;
                 text-shadow: -2px 2px #332D2D;
 
+
             }
-            
+
             input.input2button {
                 height: 70px;
                 width: 300px;
@@ -117,33 +176,27 @@ and open the template in the editor.
                 font-weight: bold;
                 color: white;
                 display: block;
-                margin-bottom: 20px;
+                /*margin-bottom: 50px;*/
                 text-shadow: -2px 2px #332D2D;
 
             }
-
-            div.buttondiv {
-                margin-bottom: 1%;
-                margin-left: 25%;
-
-            }
-
-
-
         </style>
 
     </head>
+
     <body>
 
-
+        <!-- Interface Header -->
         <header align="center" >
             <h1 style="font-family: Helvetica; font-size: 80px; color: white; text-shadow: 2px 8px #000000; margin-bottom: 5px;">FIGHTING ROBOTS</h1>
-            <h2 style="font-family: Helvetica; font-size: 40px; color: white; text-shadow: 2px 4px #000000; margin-top:auto;" >Robotic Arm Ranges</h2>
+            <h2 style="font-family: Helvetica; font-size: 40px; color: white; text-shadow: 2px 4px #000000;" >Robotic Arm Ranges</h2>
         </header>
 
 
-        <div class="outerbox" id="myForm">
+        <!-- Robotic Arm Range Control -->
+        <div class="rangeContainer" id="myForm">
 
+            <!-- Motor 1 -->
             <div class="slidecontainer">
                 <label>motor 1</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange1">
@@ -160,8 +213,7 @@ and open the template in the editor.
                 </script>
             </div>    
 
-
-
+            <!-- Motor 2 -->
             <div class="slidecontainer">
                 <label>motor 2</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange2">
@@ -178,8 +230,7 @@ and open the template in the editor.
                 </script>
             </div> 
 
-
-
+            <!-- Motor 3 -->
             <div class="slidecontainer">   
                 <label>motor 3</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange3">
@@ -196,8 +247,7 @@ and open the template in the editor.
                 </script>
             </div>
 
-
-
+            <!-- Motor 4 -->
             <div class="slidecontainer">    
                 <label>motor 4</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange4">
@@ -214,8 +264,7 @@ and open the template in the editor.
                 </script>
             </div>
 
-
-
+            <!-- Motor 5 -->
             <div class="slidecontainer">
                 <label>motor 5</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange5">
@@ -232,8 +281,7 @@ and open the template in the editor.
                 </script>
             </div>
 
-
-
+            <!-- Motor 6 -->
             <div class="slidecontainer">
                 <label>motor 6</label>
                 <input type="range" min="0" max="180" value="90" class="slider" id="myRange6">
@@ -249,62 +297,89 @@ and open the template in the editor.
                     }
                 </script>
             </div>
+
         </div>
 
 
+
+        <!-- Robot Direction Control -->
+        <div class="directionContainer" >
+             
+            <div class="directions" style="margin-top: 80px;">
+                <button class="dirbutton">Forward</button>
+            </div>
+
+            <div class="directions">
+                <button class="dirbutton" style="display: inline-block; width: 120px; margin-right: 50px;">Left</button>
+            </div>
+
+            <div class="directions">
+                <button class="dirbutton" style="display: inline-block; width: 120px;">Right</button>
+            </div>
+
+            <div class="directions">
+                <button class="dirbutton" >Backward</button>
+            </div>
+            
+
+        </div>
+
+
+        <!-- Robot Action Buttons -->
         <div class="buttondiv">
 
-            <button style="background-image: linear-gradient(to right, #B8EDE7, #218D7F);">Start</button>
+            <button class="general" style="background-image: linear-gradient(to right, #B8EDE7, #218D7F);">Start</button>
 
-            <button style="background-image: linear-gradient(to right, #FF9D9D, #BC2424);">Stop</button>
+            <button class="general" style="background-image: linear-gradient(to right, #FF9D9D, #BC2424);">Stop</button>
 
-            <button style="background-image: linear-gradient(to right, #B6E7FD, #0089C7);">Save</button>
+            <button class="general" style="background-image: linear-gradient(to right, #B6E7FD, #0089C7); ">Save</button>
 
-            <input class="input2button" style="background-image: linear-gradient(to right, #D8D8D8, #747474);" id="reset" type="button" value="Reset">
+            <input class="general, input2button" style="background-image: linear-gradient(to right, #D8D8D8, #747474);" id="reset" type="button" value="Reset">
 
+            <!-- Resetting Motor Ranges -->
             <script>
-                
+
                 var slider1 = document.getElementById("myRange1");
                 var output1 = document.getElementById("demo1");
                 output1.innerHTML = slider1.value;
-                
+
                 var slider2 = document.getElementById("myRange2");
                 var output2 = document.getElementById("demo2");
                 output2.innerHTML = slider2.value;
-                
+
                 var slider3 = document.getElementById("myRange3");
                 var output3 = document.getElementById("demo3");
                 output3.innerHTML = slider3.value;
-                
+
                 var slider4 = document.getElementById("myRange4");
                 var output4 = document.getElementById("demo4");
                 output4.innerHTML = slider4.value;
-                
+
                 var slider5 = document.getElementById("myRange5");
                 var output5 = document.getElementById("demo5");
                 output5.innerHTML = slider5.value;
-                
+
                 var slider6 = document.getElementById("myRange6");
                 var output6 = document.getElementById("demo6");
                 output6.innerHTML = slider6.value;
 
                 document.getElementById('reset').onclick = function () {
-                    
+
                     output1.innerHTML = 90;
                     document.getElementById('myRange1').value = 90;
-                    
+
                     output2.innerHTML = 90;
                     document.getElementById('myRange2').value = 90;
-                    
+
                     output3.innerHTML = 90;
                     document.getElementById('myRange3').value = 90;
-                    
+
                     output4.innerHTML = 90;
                     document.getElementById('myRange4').value = 90;
-                    
+
                     output5.innerHTML = 90;
                     document.getElementById('myRange5').value = 90;
-                    
+
                     output6.innerHTML = 90;
                     document.getElementById('myRange6').value = 90;
                 }
@@ -313,8 +388,10 @@ and open the template in the editor.
 
         </div>
 
-        
-        
+
+
+
+
         <!--
         <?php
         // put your code here
@@ -324,4 +401,5 @@ and open the template in the editor.
 
 
     </body>
+
 </html>
